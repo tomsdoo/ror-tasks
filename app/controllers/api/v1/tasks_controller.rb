56cd38e44425db30a499e51b3cf906ba6a2fa047
tomsdoo <toms@live.jp>
2026-09-10 19:30:49 +0900

    docker compose run --rm web bundle exec rails g model Task title:string completed:boolean docker compose run --rm web bundle exec rails db:migrate

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,2 +1,23 @@
-class Api::V1::TasksController < ApplicationController
+class Api::V1::TasksController < ActionController::API
+  # GET /api/v1/tasks
+  def index
+    tasks = Task.order(created_at: :desc)
+    render json: tasks, status: :ok
+  end
+
+  # POST /api/v1/tasks
+  def create
+    task = Task.new(task_params)
+    if task.save
+      render json: task, status: :created
+    else
+      render json: { errors: task.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :completed)
+  end
 end
